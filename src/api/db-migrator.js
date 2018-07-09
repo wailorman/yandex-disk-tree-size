15bdd8a72f1d795e9b9954745c363471273fc6d1
Sergey Popov <wailorman@gmail.com>
2018-07-09 21:14:10 +0300

    refactor(db-migrator): Reduce indexable columnds in Dexie

--- src/api/db-migrator.js
+++ src/api/db-migrator.js
@@ -1,18 +1,7 @@
 export const migrateDb = (db) => {
   db.version(1).stores({
-    resources:
-      'id,'
-      + 'name,'
-      + 'type,'
-      + 'path,'
-      + 'size,'
-      + 'totalChildResources,'
-      + 'loadedChildResources,'
-      + 'parentResourceId,'
-      + 'childResourcesIds,'
-      + 'allParentResourcesIds,'
-      + 'allChildResourcesIds',
+    resources: ['id', 'name', 'type', 'parentResourceId'].join(','),
   });
-}
+};
 
 export default migrateDb;
